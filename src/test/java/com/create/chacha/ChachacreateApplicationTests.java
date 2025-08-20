d7c39ce4bec044fc840045d3d1518ad807ec06de
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-08-20 15:13:18 +0900

    🎉 init: gradle/springboot 프로젝트 초기파일
    gradle/springboot 생성 시 나오는 초기파일

---
+++ src/test/java/com/create/chacha/ChachacreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.create.chacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChachacreateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
