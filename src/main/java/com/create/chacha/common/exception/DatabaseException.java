c0502762ca82b924b1f0aa2face489c5fbc55f7a
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-08-22 11:09:28 +0900

    🎉 init: API 응답 양식 및 전역 예외 처리 추가
    🎉 init: API 응답 양식 및 전역 예외 처리 추가

---
+++ src/main/java/com/create/chacha/common/exception/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.create.chacha.common.exception;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
