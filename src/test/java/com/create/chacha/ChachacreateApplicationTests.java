c2424425b1ec2a705d560feb9e78e8f8b2974c3b
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-08-25 17:57:21 +0900

    🎉 init: 테스트 기본 설정 코드 추가
    springboot test시에도 env파일을 읽어야하기 때문에 test용 코드를 추가함

--- src/test/java/com/create/chacha/ChachacreateApplicationTests.java
+++ src/test/java/com/create/chacha/ChachacreateApplicationTests.java
@@ -1,13 +1,36 @@
 package com.create.chacha;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(initializers = ChachacreateApplicationTests.EnvInitializer.class)
 class ChachacreateApplicationTests {
+
+    static class EnvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+            String jasyptPassword = dotenv.get("JASYPT_ENCRYPTOR_PASSWORD");
+            if (jasyptPassword != null) {
+                System.setProperty("JASYPT_ENCRYPTOR_PASSWORD", jasyptPassword);
+            }
+
+            String base64AesPassword = dotenv.get("DATABASE_AES_PASSWORD");
+            if (base64AesPassword != null) {
+                String decodedPassword = new String(java.util.Base64.getDecoder().decode(base64AesPassword));
+                System.setProperty("DATABASE_AES_PASSWORD", decodedPassword);
+            }
+        }
+    }
 
     @Test
     void contextLoads() {
+        // 단순 컨텍스트 로드 테스트
     }
-
 }
