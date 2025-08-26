af85ed264669a64a6f94a477666fe390c43e8103
yoonjunglv <yoonjung450@gmail.com>
2025-08-26 19:29:07 +0900

    ✨ feat: 스토어 커스텀 수정 기능

--- src/main/java/com/create/chacha/domains/seller/areas/store/custom/controller/SellerStoreCustomController.java
+++ src/main/java/com/create/chacha/domains/seller/areas/store/custom/controller/SellerStoreCustomController.java
@@ -3,6 +3,7 @@ package com.create.chacha.domains.seller.areas.store.custom.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.create.chacha.domains.seller.areas.store.custom.dto.request.StoreCustomUpdateRequestDTO;
 import com.create.chacha.domains.seller.areas.store.custom.dto.response.StoreCustomGetResponseDTO;
 import com.create.chacha.domains.seller.areas.store.custom.service.SellerStoreCustomService;
 
@@ -14,6 +15,14 @@ import lombok.RequiredArgsConstructor;
 public class SellerStoreCustomController {
 
     private final SellerStoreCustomService sellerStoreCustomService;
+    
+    // 스토어 커스텀 수정 (부분 업데이트)
+    @PatchMapping("/custom")
+    public ResponseEntity<StoreCustomGetResponseDTO> updateCustom(
+            @PathVariable("storeUrl") String storeUrl,
+            @RequestBody StoreCustomUpdateRequestDTO request) {
+        return ResponseEntity.ok(sellerStoreCustomService.updateStoreCustom(storeUrl, request));
+    }
 
     // 스토어 커스텀 조회
     @GetMapping("/custom")
---
+++ src/main/java/com/create/chacha/domains/seller/areas/store/custom/repository/StoreFontRepository.java
@@ -0,0 +1,8 @@
+package com.create.chacha.domains.seller.areas.store.custom.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.create.chacha.domains.shared.entity.store.StoreFontEntity;
+
+public interface StoreFontRepository extends CrudRepository<StoreFontEntity, Long> {
+}
