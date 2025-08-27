624af255ffe0438f3dc6cd5d3bfba74ab0815edb
yoonjunglv <yoonjung450@gmail.com>
2025-08-27 10:48:53 +0900

    ✨ feat: 판매자 상품 가격 추천

---
+++ src/main/java/com/create/chacha/domains/seller/areas/products/price/dto/response/ProductPriceRecommendSimpleResponseDTO.java
@@ -0,0 +1,12 @@
+package com.create.chacha.domains.seller.areas.products.price.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductPriceRecommendSimpleResponseDTO {
+    private Integer recommendedPrice;
+}
