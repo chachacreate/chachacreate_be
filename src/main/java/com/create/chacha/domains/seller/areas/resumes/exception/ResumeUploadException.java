d2f54f628abd0e3184f3f6689c28fbcd8dd1577e
jahuiLee <jennysky46@gmail.com>
2025-09-01 12:36:54 +0900

    ✨ feat: 판매자 이력 인증 커스텀 에러 추가

---
+++ src/main/java/com/create/chacha/domains/seller/areas/resumes/exception/ResumeUploadException.java
@@ -0,0 +1,7 @@
+package com.create.chacha.domains.seller.areas.resumes.exception;
+
+public class ResumeUploadException extends RuntimeException {
+    public ResumeUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
