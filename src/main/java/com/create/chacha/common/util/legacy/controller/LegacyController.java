14665f84245d29510b62be29c4a2091585e994fd
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-09-04 16:34:16 +0900

    ✨ feat: 레거시에 회원 아이디로 회원 정보 가져오는 api추가
    ✨ feat: 레거시에 회원 아이디로 회원 정보 가져오는 api추가

--- src/main/java/com/create/chacha/common/util/legacy/controller/LegacyController.java
+++ src/main/java/com/create/chacha/common/util/legacy/controller/LegacyController.java
@@ -2,40 +2,53 @@ package com.create.chacha.common.util.legacy.controller;
 
 import com.create.chacha.common.ApiResponse;
 import com.create.chacha.common.constants.ResponseCode;
+import com.create.chacha.config.security.SecurityUser;
 import com.create.chacha.domains.shared.entity.member.MemberAddressEntity;
 import com.create.chacha.domains.shared.entity.member.MemberEntity;
 import com.create.chacha.common.util.legacy.service.LegacyInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 @Slf4j
 public class LegacyController {
     @Autowired
     LegacyInfoService legacyInfoService;
 
     @GetMapping("/info/seller/{memberId}")
-    public ApiResponse<MemberEntity> sendMemberEntityForSeller(@PathVariable Integer memberId){
+    public ApiResponse<MemberEntity> sendMemberEntityBySellerId(@PathVariable Integer memberId){
         MemberEntity memberEntity = legacyInfoService.getSellerInfo(memberId);
         log.info(memberEntity.toString());
         return new ApiResponse<>(ResponseCode.OK, memberEntity);
     }
-    @GetMapping("/info/memberAdress/{memberId}")
+    @GetMapping("/info/memberAddress/{memberId}")
     public ApiResponse<MemberAddressEntity> sendMemberAddressEntityByMemberId(@PathVariable Integer memberId){
         MemberAddressEntity address = legacyInfoService.getMemberAddress(memberId);
         log.info(address.toString());
         return new ApiResponse<>(ResponseCode.OK, address);
     }
 
-    @GetMapping("/info/memberAdressByAddresId/{addressId}")
+    @GetMapping("/info/memberAddressByAddressId/{addressId}")
     public ApiResponse<MemberAddressEntity> sendMemberAddressEntityByMemberAddressId(@PathVariable Integer addressId){
         MemberAddressEntity address = legacyInfoService.getMemberAddressByAddressId(addressId);
         log.info(address.toString());
         return new ApiResponse<>(ResponseCode.OK, address);
     }
+
+    @GetMapping("/info/member/{memberId}")
+    public ApiResponse<MemberEntity> sendMemberEntityByMemberId(@PathVariable Integer memberId, @AuthenticationPrincipal SecurityUser user){
+        if(user.getMemberId().equals(memberId.longValue())){
+            MemberEntity memberEntity = user.getMemberEntity();
+            log.info(memberEntity.toString());
+            return new ApiResponse<>(ResponseCode.OK, memberEntity);
+        }
+        return new ApiResponse<>(ResponseCode.FAIL, null);
+    }
+
 }
