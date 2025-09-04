d65b8e270f68d94dbbe3d1711a28affcf86bb0fd
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-09-04 15:45:21 +0900

    ♻️ refactor: 레거시 주소도 설정파일에서 읽도록 수정
    ♻️ refactor: 레거시 주소도 설정파일에서 읽도록 수정

--- src/main/java/com/create/chacha/common/util/LegacyAPIUtil.java
+++ src/main/java/com/create/chacha/common/util/LegacyAPIUtil.java
@@ -1,34 +1,39 @@
 package com.create.chacha.common.util;
 
 
-import com.create.chacha.common.ApiResponse;
 import com.create.chacha.common.util.dto.LegacyResponse;
 import com.create.chacha.common.util.dto.LegacySellerDTO;
 import com.create.chacha.common.util.dto.LegacyStoreDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
 public class LegacyAPIUtil {
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${spring.legacy.api.url}")
+    private String legacyApiUrl;
+
     public LegacyStoreDTO getLegacyStoreData(String storeUrl) {
-        String url = "http://localhost:9999/legacy/info/store/" + storeUrl; // legacy API 주소
+        String url = legacyApiUrl + "/info/store/" + storeUrl; // legacy API 주소
         ResponseEntity<LegacyResponse<LegacyStoreDTO>> response =
                 restTemplate.exchange(url,
                         HttpMethod.GET,
                         null,
                         new ParameterizedTypeReference<LegacyResponse<LegacyStoreDTO>>() {}
                 );
-
+        log.debug(response.getBody().getData().toString());
         return response.getBody().getData();
     }
 
     public LegacySellerDTO getLegacySellerData(String storeUrl) {
-        String url = "http://localhost:9999/legacy/info/seller/" + storeUrl;
+        String url = legacyApiUrl + "/info/seller/" + storeUrl;
 
         ResponseEntity<LegacyResponse<LegacySellerDTO>> response =
                 restTemplate.exchange(url,
@@ -36,7 +41,7 @@ public class LegacyAPIUtil {
                         null,
                         new ParameterizedTypeReference<LegacyResponse<LegacySellerDTO>>() {}
                 );
-
+        log.debug(response.getBody().getData().toString());
         return response.getBody().getData();
     }
 }
