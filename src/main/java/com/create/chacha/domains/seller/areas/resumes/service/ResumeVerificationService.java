f30c1633dcb298101fad1288676f292eceb548d8
sehyeon <nocount334@gmail.com>
2025-09-05 16:35:49 +0900

    🐛 fix: 판매자 이력서 등록 api Legacy 연결 수정

--- src/main/java/com/create/chacha/domains/seller/areas/resumes/service/ResumeVerificationService.java
+++ src/main/java/com/create/chacha/domains/seller/areas/resumes/service/ResumeVerificationService.java
@@ -2,7 +2,9 @@ package com.create.chacha.domains.seller.areas.resumes.service;
 
 import com.create.chacha.domains.seller.areas.resumes.dto.request.ResumeRequestDTO;
 import com.create.chacha.domains.seller.areas.resumes.dto.response.ResumeResponseDTO;
+import com.create.chacha.domains.seller.areas.settlement.dto.response.ClassOptionResponseDTO;
 
 public interface ResumeVerificationService {
     ResumeResponseDTO createResume(String storeUrl, ResumeRequestDTO dto);
+    ClassOptionResponseDTO getPrefill(String storeUrl);
 }
