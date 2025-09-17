2349cea1d77172610dc11925cc89ef0431819a1f
jahuiLee <jennysky46@gmail.com>
2025-09-17 12:22:33 +0900

    🐛 fix: 클래스별 정산 이미지 안 뜨는 오류 수정

--- src/main/java/com/create/chacha/domains/seller/areas/settlement/dto/response/ClassDailySettlementResponseDTO.java
+++ src/main/java/com/create/chacha/domains/seller/areas/settlement/dto/response/ClassDailySettlementResponseDTO.java
@@ -1,16 +1,14 @@
 package com.create.chacha.domains.seller.areas.settlement.dto.response;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+
+import lombok.*;
 
 /**
  * 특정 클래스의 대표이미지 + 일별 결제금액 응답
  */
-@Getter @Builder @AllArgsConstructor @NoArgsConstructor @ToString
+@Data
+@Builder @AllArgsConstructor @NoArgsConstructor
 public class ClassDailySettlementResponseDTO {
     private Long classId;              	// 클래스 ID
     private String className;          	// 클래스명
