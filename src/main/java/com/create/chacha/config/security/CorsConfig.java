ffeadb70fcb749891e86078992a5d33220fe8c37
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-09-22 10:45:47 +0900

    🐛 fix: cors 설정 누락 해결
    patch 요청 추가

--- src/main/java/com/create/chacha/config/security/CorsConfig.java
+++ src/main/java/com/create/chacha/config/security/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
                 .allowedOrigins(allowedOrigins.toArray(new String[0])) // React, Vite에서 접근 허용, String[]으로 변경
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP 메서드 허용
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // HTTP 메서드 허용
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키, 인증 정보 허용
     }
